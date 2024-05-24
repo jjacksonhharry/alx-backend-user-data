bd4a539e74b9d845017db08cb2fc0b2a0a4286f5
Harry Muriithi <hharryjjack@gmail.com>
2024-05-25 00:02:15 +0300

    Basic - Overload current_user - and BOOM! 2

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,10 @@
 """
 class BasicAuth that inherits from Auth
 """
+import base64
 from api.v1.auth.auth import Auth
-from typing import Optional
+from models.user import User
+from typing import Optional, TypeVar
 
 
 class BasicAuth(Auth):
