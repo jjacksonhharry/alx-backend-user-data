5ff6c4e5907fcd1f71f41e049bc10310073bc90d
Harry Muriithi <hharryjjack@gmail.com>
2024-05-30 11:14:32 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,13 @@ class DB:
             raise NoResultFound("No user found with the provided arguments")
         except InvalidRequestError:
             raise InvalidRequestError("Invalid query arguments")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database based on the provided arguments
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"User has no attribute '{key}'")
+            setattr(user, key, value)
+        self._session.commit()
